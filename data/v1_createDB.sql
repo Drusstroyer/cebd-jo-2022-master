7a30cf8f9a03aa0d09228d677c08abc5c70e3a53
xaelxx14 <xaelxx14@gmail.com>
2022-11-24 11:10:54 +0100

    V1 CREATE DB (tables-views)

--- data/v1_createDB.sql
+++ data/v1_createDB.sql
@@ -1,14 +1,18 @@
 -- TODO 1.3a : Créer les tables manquantes et modifier celles ci-dessous
 CREATE TABLE LesSportifs
 (
-  numSp NUMBER(4) primary KEY,
+  numSp NUMBER(4) PRIMARY KEY,
   nomSp VARCHAR2(20) NOT NULL,
   prenomSp VARCHAR2(20) NOT NULL,
   pays VARCHAR2(20) NOT NULL,
   categorieSp VARCHAR2(10),
-  dateNaisSp DATE,
-  CONSTRAINT SP_CK1 CHECK(numSp > 0),
-  CONSTRAINT SP_CK2 CHECK(categorieSp IN ('feminin','masculin'))
+  dateNaisSp DATE NOT NULL,
+  CONSTRAINT SP_CK1 CHECK(numSp > 999),
+  CONSTRAINT SP_CK2 CHECK(numSp < 1501),
+  CONSTRAINT SP_CK3 CHECK(categorieSp) IN ('feminin','masculin')),
+  CONSTRAINT SP_NSP_FK FOREIGN KEY (numSp) REFERENCES LesInscrits(numParticipant),
+  CONSTRAINT SP_CSP1_FK FOREIGN KEY (categorieSp) REFERENCES LesEquipes(categorieEq),
+  CONSTRAINT SP_CSP2_FK FOREIGN KEY (categorieSp) REFERENCES LesEpreuves(categorieEp)
 );
 
 CREATE TABLE LesEquipes
@@ -18,15 +22,19 @@ CREATE TABLE LesEquipes
  pays VARCHAR(20),
  CONSTRAINT EQ_CK1 CHECK(numEq > 0),
  CONSTRAINT EQ_CK2 CHECK(numEq <101),
- CONSTRAINT EQ_CK2 CHECK(categorieEq IN ('feminin','masculin','mixte'))
+ CONSTRAINT EQ_CK2 CHECK(categorieEq IN ('feminin','masculin','mixte')),
+ CONSTRAINT EQ_CAT_FK FOREIGN KEY (categorieEq) REFERENCES LesEpreuves(categorieEp),
+ CONSTRAINT EQ_PAYS_FK FOREIGN KEY (pays) REFERENCES LesSportifs(pays),
+ CONSTRAINT EQ_NUM_FK FOREIGN KEY (numEq) REFERENCES LesInscrits(numParticipant)
+
 );
 
-CREATE TABLE Appartient
+CREATE TABLE LesMembres
 (
  numSp NUMBER(4),
  numEq NUMBER(4),
- CONSTRAINT AP_CK1 FOREIGN KEY (nomSp) REFERENCES LesSportifs(numSp),
- CONSTRAINT AP_CK2 FOREIGN KEY (nomEq) REFERENCES LesEquipes(numEq)
+ CONSTRAINT AP_FK1 FOREIGN KEY (nomSp) REFERENCES LesSportifs(numSp),
+ CONSTRAINT AP_FK2 FOREIGN KEY (nomEq) REFERENCES LesEquipes(numEq)
 );
 CREATE TABLE LesDisciplines
 (
@@ -49,12 +57,38 @@ CREATE TABLE LesEpreuves
   CONSTRAINT EP_CK5 FOREIGN KEY (nomDi) REFERENCES LesDisciplines(nomDi)
 );
 
-CREATE TABLE Lesinscrits
+CREATE TABLE LesResultats
 (
- NumParticipant NUMBER(4),
- numEp NUMBER(3),
- CONSTRAINT INS_CK1 FOREIGN
+    numEp NUMBER(3) PRIMARY KEY,
+    numOr NUMBER(4),
+    numArg NUMBER(4),
+    numBrze NUMBER(4),
+    CONSTRAINT RES_nEP_FK FOREIGN KEY (numEp) REFERENCES LesEpreuves(numEp),
+    CONSTRAINT RES_OR_FK FOREIGN KEY (numOr) REFERENCES LesInscrits(numParticipant),
+    CONSTRAINT RES_ARG_FK FOREIGN KEY (numArg) REFERENCES LesInscrits(numParticipant),
+    CONSTRAINT RES_BRZE_FK FOREIGN KEY (numBrze) REFERENCES LesInscrits(numParticipant)
 );
+
+CREATE TABLE LesInscrits
+(
+ numParticipant NUMBER(4),
+ numEp NUMBER(3),
+ CONSTRAINT INS_EP_FK FOREIGN KEY (numEp) REFERENCES LesEpreuves(numEp),
+
+);
+DROP VIEW IF EXISTS LesAgesSportifs
+CREATE VIEW IF NOT EXISTS LesAgesSportifs
+AS
+SELECT numSp, (YEAR(currentdate() - YEAR(dateNaisSp)  AS age
+FROM LesSportifs;
+
+DROP VIEW IF EXISTS LesNbsEquipiers
+CREATE VIEW IF NOT EXISTS LesNbsEquipiers
+AS
+SELECT numEq, COUNT(numSp)
+FROM LesMembres
+GROUP BY numEq;
+
 
 -- TODO 1.4a : ajouter la définition de la vue LesAgesSportifs
 -- TODO 1.5a : ajouter la définition de la vue LesNbsEquipiers
